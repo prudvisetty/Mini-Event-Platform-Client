469c1ca71c9b88df62d5d6b7c6726934cadd2454
PrudviNarayana <prod.prudvinarayana@gmail.com>
2025-12-16 16:39:53 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/Mini-Event-Platform-Client',
+  base: process.env.VITE_API_BASE_URL || '/Mini-Event-Platform-Client',
   server: {
     port: 5173,
   },
